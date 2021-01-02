3ef43cdbe22ef62672765946369f39844f893436
최성준 <csj2702@naver.com>
2021-01-03 00:33:29 +0900

    bakery-review  init

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import Bottom from './Page/Bottom';
+
+function App(){
+  return (
+        <NavigationContainer>
+            <Bottom />
+        </NavigationContainer>
+  );
+};
+
+export default App;
